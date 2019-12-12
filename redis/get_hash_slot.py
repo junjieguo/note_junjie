792863a6ad42b74ca82c6f1d701ea958508e2613
junjieguo <junjieguo1990@gmail.com>
2019-12-12 19:47:27 +0800

    Create get_hash_slot.py

---
+++ redis/get_hash_slot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import crc16
+import sys
+
+def get_key(key_values):
+     hash_key=(crc16.crc16xmodem(key_values))%16384
+     return hash_key
+
+if  __name__ == "__main__":
+    if len(sys.argv)==2:
+      
+        print (get_key(sys.argv[1]))
+    else:
+        print("ERROR  plase enter string")
