13406fa8aedcb0e10ea7b4b0ae9b0e0e1bc8ffb2
junjieguo <junjieguo1990@gmail.com>
2020-10-29 16:58:13 +0800

    Create string_if_in_file.py

---
+++ python/string_if_in_file.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+#coding:UTF-8
+'''
+FileName:   comm_api_for_tdw.py
+Author:     skyguo
+@contact:   skyguo@tencent.com
+@version:   1.0.0
+Description: get information from  lz
+ api
+Changelog:
+
+'''
+import argparse
+import time
+import requests 
+import json
+import datetime
+import sys
+import os
+import ast
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import os
+os.environ['NLS_LANG'] = 'Simplified Chinese_CHINA.ZHS16GBK'
+
+def  table_if_in_while_list(file_name,table_name ):
+        fh=open(file_name)
+        while fh:
+            line=fh.readline().strip('\n')
+            if not line:
+                return "False"
+                break
+            if table_name  in line:
+                #print (table_name)
+                return "True"
+                break
+            else:
+                continue 
+
+if  __name__ == "__main__":
+  
+    if len(sys.argv) < 2:
+        print ("plase  into  function name")
+        exit()
+    function_name=sys.argv[1]
+
+    file_name=sys.argv[2]
+
+    table_if_in_while_list(function_name,file_name,"skyguo")
+    print(table_if_in_while_list(file_name,"skyguo")    )
+    print(table_if_in_while_list(file_name,"skyguo1")    )    
+    print(table_if_in_while_list(file_name,"guojunjie")    )   
