7741a182b6b9bfbf05a20d8b68882043e6c5084a
junjieguo <junjieguo1990@gmail.com>
2022-03-21 10:26:07 +0800

    Create use_time.sh

---
+++ shell/use_time.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+begin_time="$1"
+
+tar_days=$2
+
+
+
+for((i=0;i<$2;i++))
+do
+        deal_day=`date  -d " ${begin_time} $i days  ago" "+%Y-%m-%d"`
+        let "bf=i+1"
+        let "af=i+2"
+        deal_yestarday=`date  -d " ${begin_time} $bf days  ago" "+%Y-%m-%d"`
+        deal_afterday=`date  -d " ${begin_time} $af days  " "+%Y-%m-%d"`
+        echo "${deal_yestarday} ${deal_day} ${deal_afterday}"
+done 
